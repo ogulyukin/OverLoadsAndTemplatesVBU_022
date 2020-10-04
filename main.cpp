93f25f35023b9f0073616eeacd9f1a6e6962aed1
ogulyukin <o.gulyukin@yandex.ru>
2020-10-04 13:47:19 +0200

    Switch to templates

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #define tab "\t"
 using namespace std;
-void FillRand(int arr[], const int n);
-void Print(const int arr[], const int n);
-void FillRand(double arr[], const int n);
-void Print(const double arr[], const int n);
+template <typename T>void FillRand(T arr[], const int n);
+template <typename T>void Print(const T arr[], const int n);
+
 
 int main() {
     setlocale(LC_ALL, "");
@@ -18,23 +17,12 @@ int main() {
     Print(brr,m);
     return 0;
 }
-void FillRand(int arr[], const int n){
+template <typename T>void FillRand(T arr[], const int n){
     for (int i = 0; i < n; i++){
         arr[i] = rand()%100;
     }
 }
-void Print(const int arr[], const int n){
-    for (int i = 0; i < n; i++){
-        cout << arr[i] << tab;
-    }
-    cout << endl;
-}
-void FillRand(double arr[], const int n){
-    for (int i = 0; i < n; i++){
-        arr[i] = rand()%1000/(double)100;
-    }
-}
-void Print(const double arr[], const int n){
+template <typename T>void Print(const T arr[], const int n){
     for (int i = 0; i < n; i++){
         cout << arr[i] << tab;
     }
